07a05929c7c73846d7baeafe6bd352a9c4515917
Jingwei Xu <fxkxjw@gmail.com>
2014-10-27 17:56:41 -0400

    doventstat

--- test.c
+++ test.c
@@ -4,6 +4,57 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 
+//test the doeventstat
+int main(){
+        struct prinfo *p =(struct prinfo*) malloc(sizeof(struct prinfo));
+        uid_t *UID = 0;
+        gid_t *GID = 0;
+        int *UIDFlag = 0;
+        int *GIDFlag = 0;
+        pid_t pid;
+        int eid;
+        pid = getpid();
+        printf("The process id is %d\nUID is %d\nGID is %d\n",pid,getuid(),getgid());
+
+        eid = syscall(181);     //Creat event
+        if(syscall(214, eid, UID, GID, UIDFlag, GIDFlag) == -1){        //Read the info of the event with given eid
+                printf("doeventstat fail\n");
+                return 0;
+        }
+        printf("The first event ID is: %d\n UID: %d; UIDFlag: %d; GID: %d; GIDFlag: %d;",eid, *UID, *UIDFlag, *GID, *GIDFlag);
+
+
+//Change UID to 100, GID to 101, UIDFlag to 102, GIDFlag to 103
+        if(syscall(205, eid, 100, 101) == -1){  //Change the UID to 100, GID to 101
+                printf("doeventchown fail\n");
+                return 0;
+        }
+        if(syscall(214, eid, UID, GID, UIDFlag, GIDFlag) == -1){        //Read the info of the event with given eid
+                printf("doeventstat fail\n");
+                return 0;
+        }
+        printf("The first event ID is: %d\n UID: %d; UIDFlag: %d; GID: %d; GIDFlag: %d;",eid, *UID, *UIDFlag, *GID, *GIDFlag);
+
+        if(syscall(211, eid, 102, 103) == -1){  //Change the UIDFlag to 102, GIDFlag to 103
+                printf("doeventchmod fail\n");
+                return 0;
+        }
+        if(syscall(214, eid, UID, GID, UIDFlag, GIDFlag) == -1){        //Read the info of the event with given eid
+                printf("doeventstat fail\n");
+                return 0;
+        }
+        printf("The first event ID is: %d\n UID: %d; UIDFlag: %d; GID: %d; GIDFlag: %d;",eid, *UID, *UIDFlag, *GID, *GIDFlag);
+
+
+
+return 0;
+}
+
+
+
+
+
+
 // test the doeventinfo 
 int main(int argc, char **argv){
         int num,num2,i;
